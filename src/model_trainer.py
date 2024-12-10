c1747e644dda84810b6e847151069c9c6d20542a
Anton Vlasov <nocsland@gmail.com>
2024-12-10 20:41:01 +0400

    Update comments

--- src/model_trainer.py
+++ src/model_trainer.py
@@ -18,7 +18,7 @@ from transformers import (
     EarlyStoppingCallback,
 )
 
-# Настройка логирования
+# Настройка логирования: создание директории и файла для логов
 log_dir = 'logs'
 log_file = 'model_trainer.log'
 os.makedirs(log_dir, exist_ok=True)
@@ -29,48 +29,52 @@ logging.basicConfig(
     filename=log_path
 )
 
-# Отключить все предупреждения
+# Отключение всех предупреждений для упрощения вывода
 warnings.filterwarnings("ignore")
 
 
-# Функция для вычисления перплексии
+# Функция для вычисления перплексии модели
 def compute_perplexity(logits, labels):
+    # Вычисление логарифмов вероятностей и функции потерь (кросс-энтропия)
     log_probs = torch.nn.functional.log_softmax(logits, dim=-1)
     cross_entropy = torch.nn.functional.cross_entropy(log_probs.view(-1, log_probs.size(-1)), labels.view(-1),
                                                       reduction='none')
-    perplexity = torch.exp(cross_entropy.mean())
+    perplexity = torch.exp(cross_entropy.mean())  # Вычисление перплексии
     return perplexity
 
 
+# Класс для кастомного колбэка в процессе обучения
 class TrainingCallback(TrainerCallback):
     def on_log(self, args, state, control, logs=None, **kwargs):
         if logs:
+            # Логирование перплексии на основе eval_loss
             if "eval_loss" in logs:
                 eval_loss = logs["eval_loss"]
                 perplexity = np.exp(eval_loss)
                 logs["eval_perplexity"] = perplexity
                 logging.info(f"Perplexity: {perplexity}")
 
-            # Логируем весь словарь logs
+            # Логирование всех метрик, доступных в logs
             logging.info(f"Logs: {logs}")
 
 
+# Основной класс для обучения модели
 class FineTuner:
     def __init__(self, model_name='ai-forever/rugpt3small_based_on_gpt2', cache_dir='data/model/model_cache',
                  model_path='data/model', data_path='data/dataset'):
-        self.model_path = Path(model_path)
-        self.data_path = Path(data_path)
+        self.model_path = Path(model_path)  # Директория для сохранения модели
+        self.data_path = Path(data_path)  # Директория для хранения данных
 
-        # Указываем точный путь для кэша в data/model/model_cache
+        # Создание директории для кэша
         self.cache_dir = Path(cache_dir)
-        os.makedirs(self.cache_dir, exist_ok=True)  # Создаем директорию для кэша, если она не существует
+        os.makedirs(self.cache_dir, exist_ok=True)
 
-        # Загружаем модель и токенизатор с использованием правильного пути для кэша
+        # Загрузка предобученной модели и токенизатора
         self.tokenizer = GPT2Tokenizer.from_pretrained(model_name, cache_dir=str(self.cache_dir))
         self.model = GPT2LMHeadModel.from_pretrained(model_name, cache_dir=str(self.cache_dir))
 
     def prepare_data(self, df):
-        """Подготовка данных и сохранение их в файл."""
+        """Подготовка данных для обучения: создание входного и выходного текста."""
         df['input'] = df.apply(
             lambda
                 row: f"<name_ru> {row['name_ru']} <rubrics> {row['rubrics']} <rating> {row['rating']} <address> {row['address']} {self.tokenizer.eos_token}",
@@ -79,6 +83,7 @@ class FineTuner:
         df['output'] = df.apply(lambda row: f"<text> {row['text']} {self.tokenizer.eos_token}", axis=1)
 
         dataset_path = self.data_path / 'full_dataset.txt'
+        # Сохранение объединенных текстов в файл
         with dataset_path.open('w', encoding='utf-8') as file:
             for input_text, target_text in zip(df['input'], df['output']):
                 file.write(input_text + ' ' + target_text + '\n')
@@ -87,20 +92,20 @@ class FineTuner:
         return dataset_path
 
     def split_dataset(self, input_file, train_file, val_file, test_file, train_size=0.8, val_size=0.1, test_size=0.1):
-        """Разделяет датасет на тренировочную, валидационную и тестовую выборки."""
+        """Разделение данных на тренировочную, валидационную и тестовую выборки."""
         with open(input_file, 'r', encoding='utf-8') as f:
             lines = f.readlines()
 
+        # Разделение на тренировочный набор и временный набор для валидации и теста
         train_lines, temp_lines = train_test_split(lines, train_size=train_size, random_state=42)
         val_lines, test_lines = train_test_split(temp_lines, train_size=val_size / (val_size + test_size),
                                                  random_state=42)
 
+        # Сохранение выборок в отдельные файлы
         with open(train_file, 'w', encoding='utf-8') as f:
             f.writelines(train_lines)
-
         with open(val_file, 'w', encoding='utf-8') as f:
             f.writelines(val_lines)
-
         with open(test_file, 'w', encoding='utf-8') as f:
             f.writelines(test_lines)
 
@@ -109,21 +114,20 @@ class FineTuner:
 
     def fine_tune(self, dataset_path, output_name='fine_tuned_geo_reviews_model', num_train_epochs=5,
                   per_device_train_batch_size=16, learning_rate=5e-5, save_steps=10_000):
-        """Тренировка модели на полном датасете, но с использованием разделенных данных для оценки."""
+        """Процесс тонкой настройки модели на данных."""
         logging.info("Starting fine-tuning process.")
         full_dataset_path = dataset_path
         train_dataset_path = self.data_path / 'train_dataset.txt'
         val_dataset_path = self.data_path / 'val_dataset.txt'
         test_dataset_path = self.data_path / 'test_dataset.txt'
 
-        # Разделение датасета на тренировочную, валидационную и тестовую выборки
-        self.split_dataset(full_dataset_path, train_dataset_path, val_dataset_path, test_dataset_path, train_size=0.8,
-                           val_size=0.1, test_size=0.1)
+        # Разделение датасета на части
+        self.split_dataset(full_dataset_path, train_dataset_path, val_dataset_path, test_dataset_path)
 
-        # Загрузка данных
+        # Подготовка датасетов
         train_dataset = TextDataset(
             tokenizer=self.tokenizer,
-            file_path=str(full_dataset_path),  # Полный датасет используется для тренировки
+            file_path=str(full_dataset_path),
             block_size=256
         )
         eval_dataset = TextDataset(
@@ -132,10 +136,12 @@ class FineTuner:
             block_size=256
         )
 
+        # Настройка data collator для языкового моделирования
         data_collator = DataCollatorForLanguageModeling(
             tokenizer=self.tokenizer, mlm=False
         )
 
+        # Параметры обучения
         training_args = TrainingArguments(
             output_dir=str(self.model_path / output_name),
             overwrite_output_dir=True,
@@ -158,7 +164,7 @@ class FineTuner:
             weight_decay=0.01,
         )
 
-        # Добавление callback для перплексии
+        # Настройка Trainer
         trainer = Trainer(
             model=self.model,
             args=training_args,
@@ -171,7 +177,7 @@ class FineTuner:
         logging.info("Training started.")
         trainer.train()
 
-        # Оценка модели на тестовом наборе
+        # Оценка модели на тестовых данных
         logging.info("Evaluating the model on the test dataset...")
         test_dataset = TextDataset(
             tokenizer=self.tokenizer,
@@ -179,31 +185,32 @@ class FineTuner:
             block_size=256
         )
         test_results = trainer.evaluate(test_dataset)
-
         logging.info(f"Test Results: {test_results}")
 
-        # Сохранение обученной модели и токенизатора
+        # Сохранение обученной модели
         logging.info("Saving the fine-tuned model...")
         self.model.save_pretrained(str(self.model_path / output_name))
         self.tokenizer.save_pretrained(str(self.model_path / output_name))
 
 
 if __name__ == "__main__":
+    # Основные пути и файл очищенных данных
     DATA_PATH = 'data/dataset'
     MODEL_PATH = 'data/model'
     CLEANED_DATA_FILE = 'geo_reviews_cleaned.json'
 
+    # Инициализация FineTuner
     fine_tuner = FineTuner(model_path=MODEL_PATH)
     cleaned_data_path = Path(DATA_PATH) / CLEANED_DATA_FILE
-    df = pd.read_json(cleaned_data_path)
 
-    # Подготовка данных
+    # Загрузка и подготовка данных
+    df = pd.read_json(cleaned_data_path)
     dataset_path = fine_tuner.prepare_data(df)
 
-    # Тренировка и оценка модели
+    # Запуск обучения модели
     fine_tuner.fine_tune(
         dataset_path=dataset_path,
-        output_name="fine_tuned_geo_reviews_model",  # Имя для сохранения модели
+        output_name="fine_tuned_geo_reviews_model",
         num_train_epochs=10,
         per_device_train_batch_size=16,
         learning_rate=5e-5
